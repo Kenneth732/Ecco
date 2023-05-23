4f2868179a03050c48fd96a0799012de448b0c67
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-05-23 12:45:16 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# rails g resource Item name image price:integer description category 
+
+# rails g controller Sessions --no-test-framework
+# rails g controller Users --no-test-framework
+# rails g resource Users username password_digest --no-test-framework
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            render json: user, status: :created
+        else
+            render json: { errors: user.errors.full_message }, status: :unprocessable_entity
+        end
+    end
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
+    end
+end
